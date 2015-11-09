169991f12344733563cd22e2f4b23fe57c61745a
James Herring <james.richard.herring@gmail.com>
2015-11-08 21:49:58 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,22 @@
+;; Add path for custom modules to load path
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+;; Add MELPA
+(require 'setup-melpa)
+
+;; Add general (non-package) emacs customizations
+
+(require 'setup-applications)
+(require 'setup-communication)
+(require 'setup-convenience)
+(require 'setup-data)
+(require 'setup-development)
+(require 'setup-editing)
+(require 'setup-environment)
+(require 'setup-external)
+(require 'setup-faces-and-ui)
+(require 'setup-files)
+(require 'setup-help)
+(require 'setup-programming)
+(require 'setup-text)
+(require 'setup-local)
